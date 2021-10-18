b300233fb76d3bce8cc046c62e036c12fe32a892
gabbar23 <amansaini842@gmail.com>
2021-10-18 11:50:10 +0530

    version 1

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager,UserMixin,login_required,logout_user,login_user,current_user
+
 
 app=Flask(__name__)
 app.config["SECRET_KEY"]="SECRET"
+app.config["SQLALCHEMY_DATABASE_URI"]
 
 @app.route('/')
-def index_get():
-    return render_template("chat.html")
+def index():
+    return render_template("index.html")
 
 if __name__=="__main__":
     app.run(debug=True)
