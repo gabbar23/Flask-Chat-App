cf32597afeeba7065a0b02f940fb6ffb888e0a8c
gabbar23 <amansaini842@gmail.com>
2021-10-22 23:36:07 +0530

    bug fixed

--- app.py
+++ app.py
@@ -13,12 +13,17 @@ import os
 
 app=Flask(__name__)
 app.config["SECRET_KEY"]=os.environ.get("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
 app.config["SESSION_TYPE"]='filesystem'
 
 
+uri =  os.environ.get("DATABASE_URL")
+if uri:
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = uri or "sqlite:///users_data.db"
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 Session(app)
 socketio = SocketIO(app, manage_session=False)
 
