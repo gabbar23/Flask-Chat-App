8bd2a0601f36c48de6ee9ff518a8e5de4114fd04
gabbar23 <amansaini842@gmail.com>
2021-10-22 23:26:45 +0530

    Final

--- app.py
+++ app.py
@@ -18,13 +18,8 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
 app.config["SESSION_TYPE"]='filesystem'
 
 
-uri =  os.environ.get("DATABASE_URL")
-if uri:
-    if uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgresql://", 1)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = uri or "sqlite:///user_data.db"
-
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
 Session(app)
 socketio = SocketIO(app, manage_session=False)
 
